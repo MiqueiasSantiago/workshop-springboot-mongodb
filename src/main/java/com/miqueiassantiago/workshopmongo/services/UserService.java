7cb4b24829d42fc60649103c31dff6fa2cd226f6
Miqueias Santiago <Miqueiasengsoft@gmail.com>
2024-06-27 12:14:18 -0300

    User insertion with POST

--- src/main/java/com/miqueiassantiago/workshopmongo/services/UserService.java
+++ src/main/java/com/miqueiassantiago/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.miqueiassantiago.workshopmongo.domain.User;
+import com.miqueiassantiago.workshopmongo.dto.UserDTO;
 import com.miqueiassantiago.workshopmongo.repository.UserRepository;
 import com.miqueiassantiago.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -25,4 +26,11 @@ public class UserService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
 	}
 	
+	public User insert(User obj) {
+		return repository.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDTO) {
+		return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
+	}
 }
