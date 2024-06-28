a95507980c45356b2153753b2755d0ff502cff47
Miqueias Santiago <Miqueiasengsoft@gmail.com>
2024-06-28 17:17:09 -0300

    Query with multiple criteria

--- src/main/java/com/miqueiassantiago/workshopmongo/config/Instantiation.java
+++ src/main/java/com/miqueiassantiago/workshopmongo/config/Instantiation.java
@@ -25,8 +25,8 @@ public class Instantiation implements CommandLineRunner{
 	private PostRepository postRepository;
 	
 	@Override
-	public void run(String... args) throws Exception {
-
+	public void run(String... arg0) throws Exception {
+		
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
 		
@@ -38,22 +38,20 @@ public class Instantiation implements CommandLineRunner{
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
-		
-		Post post1 = new Post(null, sdf.parse("2018/03/21"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
-		Post post2 = new Post(null, sdf.parse("2018/03/23"),  "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
-	
-		CommentDTO c1 = new CommentDTO("Boa viagem mano!", sdf.parse("2018/03/21"), new AuthorDTO(alex));
-		CommentDTO c2 = new CommentDTO("Aproveite!", sdf.parse("2018/03/22"), new AuthorDTO(bob));
-		CommentDTO c3 = new CommentDTO( "Tenha um ótimo dia!", sdf.parse("2018/03/23"), new AuthorDTO(alex));
+
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+
+		CommentDTO c1 = new CommentDTO("Boa viagem mano!", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+		CommentDTO c2 = new CommentDTO("Aproveite", sdf.parse("22/03/2018"), new AuthorDTO(bob));
+		CommentDTO c3 = new CommentDTO("Tenha um ótimo dia!", sdf.parse("23/03/2018"), new AuthorDTO(alex));
 		
 		post1.getComments().addAll(Arrays.asList(c1, c2));
-		post2.getComments().add(c3);
+		post2.getComments().addAll(Arrays.asList(c3));
 		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 		
 		maria.getPosts().addAll(Arrays.asList(post1, post2));
-		
 		userRepository.save(maria);
-
 	}
 }
--- src/main/java/com/miqueiassantiago/workshopmongo/resources/PostResource.java
+++ src/main/java/com/miqueiassantiago/workshopmongo/resources/PostResource.java
@@ -1,13 +1,12 @@
 package com.miqueiassantiago.workshopmongo.resources;
 
-import java.net.URLDecoder;
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -36,4 +35,15 @@ public class PostResource {
 		return ResponseEntity.ok().body(post);
 	}
 	
+	@GetMapping(value = "/fullsearch")
+ 	public ResponseEntity<List<Post>> fullSearch(
+ 			@RequestParam(value="text", defaultValue="") String text,
+ 			@RequestParam(value="minDate", defaultValue="") String minDate,
+ 			@RequestParam(value="maxDate", defaultValue="") String maxDate) {
+		text = URL.decodeParam(text);
+		Date min = URL.convertDate(minDate, new Date(0L));
+		Date max = URL.convertDate(maxDate, new Date());
+		List<Post> list = service.fullSearch(text, min, max);
+		return ResponseEntity.ok().body(list);
+	} 
 }
