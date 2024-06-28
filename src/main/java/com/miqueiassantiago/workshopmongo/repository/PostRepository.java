18e79e48db7b2a795c86e23a090289f0c74c8966
Miqueias Santiago <Miqueiasengsoft@gmail.com>
2024-06-28 16:24:16 -0300

    Simple query with @Query

--- src/main/java/com/miqueiassantiago/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/miqueiassantiago/workshopmongo/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.miqueiassantiago.workshopmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.miqueiassantiago.workshopmongo.domain.Post;
@@ -10,6 +11,9 @@ import com.miqueiassantiago.workshopmongo.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
 	
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> soarchTitle(String text);
+	
 	List<Post> findByTitleContainingIgnoreCase(String text);
-
+	
 }
